70f5fd1414f36b14bb8e6a78a1fa1534e3839774
sivchheng16 <kheangsivechheng@gmail.com>
2026-05-30 07:56:53 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import Navbar from "../component/Navbar";
+import Image from "next/image";
+import cover from "../public/cover.png";
 export default function Home() {
   return (
-    <div>
-      <Navbar />
+    <div className=" justify-center items-center ">
+      <div>
+        <Navbar />
+      </div>
+
+      <div className="container m-auto relative max-w-7xl h-[100vh] bg-red-50  flex flex-row justity-center text-center ">
+        <div className="bg-red-50 h-fit w-fit absolute top-20 ">
+          <Image src={cover} alt="cover" />
+        </div>
+      </div>
     </div>
   );
 }
