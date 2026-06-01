d566149fce4d06def32561bd316b7844a9accd9b
sivchheng16 <kheangsivechheng@gmail.com>
2026-06-01 11:09:39 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,41 @@
 import Navbar from "../component/Navbar";
 import Image from "next/image";
-import cover from "../public/cover.png";
+import cover from "../public/cover1.png";
+import { MagicCard } from "@/components/ui/magic-card";
 export default function Home() {
   return (
-    <div className=" justify-center items-center ">
+    <div className=" justify-center items-center h-[100vh] w-full ">
       <div>
         <Navbar />
       </div>
 
-      <div className="container m-auto relative max-w-7xl h-[100vh] bg-red-50  flex flex-row justity-center text-center ">
-        <div className="bg-red-50 h-fit w-fit absolute top-20 ">
-          <Image src={cover} alt="cover" />
+      <div className="container m-auto relative max-w-6xl bg-red-50  flex flex-col justity-center text-center  ">
+        <div className=" w-full h-2/3 flex items-center justify-center pt-20 pb-10">
+          <div className="rounded-2xl w-full">
+            <Image
+              src={cover}
+              alt="cover"
+              className="object-cover w-full rounded-2xl"
+            />
+          </div>
+        </div>
+        <div className="items-start flex max-w-8xl justify-start">
+          <div className="gap-10 flex flex-col ">
+            <div className="text-left">
+              <h2 className="text-3xl font-black">KOOMPI App,</h2>
+              <p className="text-lg tracking-width">
+                You might be like to play these game.
+              </p>
+            </div>
+            <div className="grid grid-cols-5 w-full items-center justify-center">
+              <MagicCard>
+                <div className="p-4">
+                  <Image src={cover} alt="Typing code game" />
+                  <span>Hover me</span>
+                </div>
+              </MagicCard>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -4,19 +4,19 @@ import logo from "../public/kapp.png";
 
 export default function Navbar() {
   return (
-    <div className="fixed h-fit top-0 w-full max-w-full inset-0 px-5 py-2 flex justify-between gap-10 z-100 bg-background">
-      <div className="flex gap-2 justify-center items-center text-black">
-        <h1 className="font-bold text-3xl">Kapp</h1>
-      </div>
+    <div className="fixed h-fit top-0 w-full max-w-full inset-0 px-16 py-2 flex justify-between gap-10 z-100 bg-background">
+        <div className="flex gap-2 justify-center items-center text-black">
+          <h1 className="font-bold text-3xl">Kapp</h1>
+        </div>
 
-      <div className="flex gap-2 items-center ">
-        <button className="py-1 px-4 rounded-2xl hover:bg-muted-foreground hover:text-white ">
-          Login
-        </button>
-        <button className="py-1 px-4 bg-dark-blue rounded-2xl border-2 text-white">
-          Sign Up
-        </button>
-      </div>
+        <div className="flex gap-2 items-center ">
+          <button className="py-1 px-4 rounded-2xl hover:bg-muted-foreground hover:text-white ">
+            Login
+          </button>
+          <button className="py-1 px-4 bg-dark-blue rounded-2xl border-2 text-white">
+            Sign Up
+          </button>
+        </div>
     </div>
   );
 }
